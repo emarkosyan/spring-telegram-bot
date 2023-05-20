d284cc35bab831797196d094a5ad6dcef375399f
Cerbinn <erikmarkosyan2003@gmail.com>
2023-05-20 18:47:37 +0300

    Init spring bot skeleton

---
+++ src/test/java/com/github/emarkosyan/stb/SpringTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.emarkosyan.stb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
